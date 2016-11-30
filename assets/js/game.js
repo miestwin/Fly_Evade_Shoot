cfa5c991783ae31dfc2508b7ceb00e74cec71441
Radosław Chełstowski <chelstowski.radoslaw@gmail.com>
2016-11-30 15:30:50 +0100

    Eksplozje gotowe

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,30 +2,8 @@ var player, playerDamage = 1, cursors,
     bullets, bulletTime = 0,
     fireButton, friendAndFoe,
     asteroids, maxAsteroids = 3,
-    ufos, maxUfos = 4, ufoBullets;
-
-function Player() {
-    this.health = 5;
-    this.damage = 1;
-}
-
-function Asteroid() {
-    this.health = 50;
-}
-
-function Ufo() {
-    this.health = 30;
-    this.damage = 2;
-}
-
-function Jet() {
-    this.health = 5;
-    this.damage = 1;
-}
-
-function Kamikaze() {
-    this.health = 1;
-}
+    ufos, maxUfos = 4, ufoBullets,
+    explosions;
 
 var Game = {
     preload: function() {
@@ -34,45 +12,23 @@ var Game = {
         game.load.image('asteroid', 'assets/images/asteroid_1.png');
         game.load.image('bullet', 'assets/images/bulletSmall.png');
         game.load.image('ufo', 'assets/images/ufoEnemy.png');
+        game.load.spritesheet('kaboom', 'assets/images/explode.png', 128, 128);
     },
 
     create: function() {
         game.add.tileSprite(0, 0, 2400, 1800, 'background');
-
-        bullets = game.add.group();
-        bullets.enableBody = true;
-        bullets.physicsBodyType = Phaser.Physics.ARCADE;
-        bullets.createMultiple(30, 'bullet');
-        bullets.setAll('anchor.x', 0.5);
-        bullets.setAll('anchor.y', 0.5);
-        bullets.setAll('outOfBoundsKill', true);
-        bullets.setAll('checkWorldBounds', true);
-
         game.world.setBounds(0, 0, 2400, 1800);
 
-        player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
-        player.scale.setTo(0.5,0.5);
-        player.anchor.set(0.5,0.5);
-        game.physics.arcade.enable(player);
-        player.body.drag.set(70);
-        player.body.maxVelocity.set(300);
-        player.body.collideWorldBounds = true;
-        player.body.bounce.set(0.5);
+        createPlayer();
 
         game.camera.follow(player);
-        
+
+        createAsteroids();
+        createUfo();
+
+        createExplosion();
+
         cursors = game.input.keyboard.createCursorKeys();
-
-        asteroids = game.add.group();
-        asteroids.enableBody = true;
-        asteroids.physicsBodyType = Phaser.Physics.ARCADE;
-        spawnAsteroids();
-
-        ufos = game.add.group();
-        ufos.enableBody = true;
-        ufos.physicsBodyType = Phaser.Physics.ARCADE;
-        spawnUfos();
-
         fireButton = this.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
     },
 
@@ -104,22 +60,45 @@ var Game = {
             fireBullet();
         }
 
-        // if (asteroids.countLiving() < maxAsteroids)
-        // {
-        //     spawnAsteroids();
-        // }
-
         game.physics.arcade.overlap(bullets, asteroids, collisionHandler, null, this);
+        game.physics.arcade.overlap(player, asteroids, collisionWithAsteroids, null, this);
         game.physics.arcade.overlap(bullets, ufos, collisionHandler, null, this);
         
     }
 };
 
+function createPlayer() {
+    player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
+    player.health = 5;
+    player.scale.setTo(0.5,0.5);
+    player.anchor.set(0.5,0.5);
+    game.physics.arcade.enable(player);
+    player.body.drag.set(70);
+    player.body.maxVelocity.set(300);
+    player.body.collideWorldBounds = true;
+    player.body.bounce.set(0.5);
+
+    bullets = game.add.group();
+    bullets.enableBody = true;
+    bullets.physicsBodyType = Phaser.Physics.ARCADE;
+    bullets.createMultiple(30, 'bullet');
+    bullets.setAll('anchor.x', 0.5);
+    bullets.setAll('anchor.y', 0.5);
+    bullets.setAll('outOfBoundsKill', true);
+    bullets.setAll('checkWorldBounds', true);
+}
+
+function createAsteroids() {
+    asteroids = game.add.group();
+    asteroids.enableBody = true;
+    asteroids.physicsBodyType = Phaser.Physics.ARCADE;
+    spawnAsteroids();
+}
+
 function spawnAsteroids() {  
     for(let i = 0; i < maxAsteroids; i++) {
         var asteroid = asteroids.create(360 + Math.random() * 400, 120 + Math.random() * 400, 'asteroid');
-        asteroid.hp = 50; 
-        asteroids.points = 200; 
+        asteroid.health = 50;
         asteroid.body.angularVelocity = game.rnd.integerInRange(0, 200);
         asteroid.anchor.set(0.5,0.5);
         asteroid.body.collideWorldBounds = true;
@@ -130,20 +109,82 @@ function spawnAsteroids() {
     }
 }
 
+function createExplosion(func) {
+    explosions = game.add.group();
+    explosions.createMultiple(60, 'kaboom');
+    explosions.forEach(function(explosion) {
+        explosion.anchor.x = 0.5;
+        explosion.anchor.y = 0.5;
+        explosion.animations.add('kaboom');
+    }, this);
+}
+
+function createUfo() {
+    ufos = game.add.group();
+    ufos.enableBody = true;
+    ufos.physicsBodyType = Phaser.Physics.ARCADE;
+    spawnUfos();
+}
+
 function spawnUfos() {
     for(let i = 0; i < maxUfos; i++) {
         var ufo = ufos.create(360 + Math.random() * 400, 120 + Math.random() * 400, 'ufo');
+        ufo.health = 30;
         ufo.body.angularVelocity = game.rnd.integerInRange(0, 200);
         ufo.anchor.set(0.5,0.5);
         ufo.body.collideWorldBounds = true;
         ufo.body.bounce.set(0.5);
+        ufo.animations.add('kaboom',[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15], 30, false);
         var randomAngle = game.math.degToRad(game.rnd.angle());
         var randomVelocity = game.rnd.integerInRange(50, 150);
         game.physics.arcade.velocityFromRotation(randomAngle, randomVelocity, ufo.body.velocity);
     }
 }
 
+function fireBullet () {
+ 
+    if (game.time.now > bulletTime)
+    {
+        bullet = bullets.getFirstExists(false);
+ 
+        if (bullet)
+        {
+            var length = player.width * 0.5;
+            var x = player.x + (Math.cos(player.rotation) * length);
+            var y = player.y + (Math.sin(player.rotation) * length);
+               
+            bullet.reset(x, y);
+            bullet.rotation = player.rotation;
+               
+            game.physics.arcade.velocityFromRotation(player.rotation, 500, bullet.body.velocity); //speed in the middle
+            bulletTime = game.time.now + 200;
+        }
+    }
+ 
+}
+
 function collisionHandler (bullet, enemy) {
+    var explosionBullet, explosionEnemy;
+    explosionBullet = explosions.getFirstExists(false);
+    explosionBullet.scale.setTo(0.3,0.3);
+    explosionBullet.reset(bullet.body.x, bullet.body.y);
+    explosionBullet.play('kaboom', 100, false, true);
+
     bullet.kill();
-    enemy.kill();
+    enemy.damage(playerDamage);
+
+    if(enemy.health <= 0) {
+        explosionEnemy = explosions.getFirstExists(false);
+        explosionEnemy.scale.setTo(1.2,1.2);
+        explosionEnemy.reset(bullet.body.x, bullet.body.y);
+        explosionEnemy.play('kaboom', 30, false, true);
+    }
+}
+
+function collisionWithAsteroids (obj, asteroid) {
+    asteroid.damage(obj.health);
+    obj.damage(obj.health);
+    var explosion = explosions.getFirstExists(false);
+    explosion.reset(obj.body.x, obj.body.y);
+    explosion.play('kaboom', 30, false, true);
 }
